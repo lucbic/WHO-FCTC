c9c24543457afeef09ae3e4345d643a334eefe07
Lucas Bicudo <lucbic@gmail.com>
2023-12-20 10:17:54 -0300

    Add Chinese lang to About page

--- src/content/index.js
+++ src/content/index.js
@@ -2,10 +2,12 @@ import en from './en'
 import ar from './ar'
 import fr from './fr'
 import ru from './ru'
+import zhCN from './zhCN'
 
 export default {
   en,
   ar,
   fr,
-  ru
+  ru,
+  zhCN
 }
---
+++ src/content/zhCN/App.js
@@ -0,0 +1,45 @@
+export default {
+  header: {
+    home: {
+      title: '主页'
+    },
+    about: {
+      title: '关于'
+    },
+    scenarios: {
+      title: '情景',
+      scenario: {
+        one: {
+          title: '情景1：',
+          description: '医疗费用回收',
+        },
+        two: {
+          title: '情景2：',
+          description: '诉诸司法',
+        },
+        three: {
+          title: '情景3：',
+          description: '公益诉讼',
+        },
+      }
+    },
+    resources: {
+      title: '资源'
+    },
+    contactUs: {
+      title: '联系我们'
+    },
+  },
+
+  footer: {
+    about: {
+      title: '关于'
+    },
+    resources: {
+      title: '资料'
+    },
+    contact: {
+      title: '联系'
+    },
+  }
+}
